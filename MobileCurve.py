f1c49c88b1900230eca6f047524421a31c3db7dc
DarshSikka <67545017+DarshSikka@users.noreply.github.com>
2021-01-05 23:03:16 +0530

    Delete MobileCurve.py

--- MobileCurve.py
+++
@@ -1,8 +0,0 @@
-import pandas
-import plotly.figure_factory as px
-import csv
-df=pandas.read_csv('MobileNetwork.csv')
-list_df=df['Mobile Brand'].tolist()
-print(list_df)
-fig=px.create_distplot([df['Avg Rating'].tolist()], ['Different Brands as per serial number in list given in dialogue'], show_hist=False)
-fig.show()
