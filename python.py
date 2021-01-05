adac70341b3fc0d022088983e6d8bf7190e68e4e
DarshSikka <67545017+DarshSikka@users.noreply.github.com>
2021-01-05 23:13:14 +0530

    Delete python.py

--- python.py
+++
@@ -1,9 +0,0 @@
-class dog():
-    def __init__(self, color, hairstyle, breed):
-        self.color=color
-        self.hairstyle=hairstyle
-        self.breed=breed
-    def showcolor(self):
-        print(self.color)
-bruno=dog("red", "curly", "lebra")
-bruno.showcolor()
